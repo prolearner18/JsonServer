d2d751b0973d7bd89a73cd805e6a4fc6bbfeb710
lucifa7 <1539601747@qq.com>
2018-04-17 11:28:15 +0800

    添加项目文件。

---
+++ JsonServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JsonServer.Startup))]
+namespace JsonServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
